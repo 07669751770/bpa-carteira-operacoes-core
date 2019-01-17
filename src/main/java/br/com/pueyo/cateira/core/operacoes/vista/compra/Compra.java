d2c51daac7560b152b411f2e5a9750d12ed9c81d
BrunoNit <pueyonit@gmail.com>
2019-01-17 16:22:46 -0200

    Correção

--- src/main/java/br/com/pueyo/cateira/core/operacoes/vista/compra/Compra.java
+++ src/main/java/br/com/pueyo/cateira/core/operacoes/vista/compra/Compra.java
@@ -1,8 +1,16 @@
 package br.com.pueyo.cateira.core.operacoes.vista.compra;
 
+import java.math.BigDecimal;
+
 import br.com.pueyo.cateira.core.operacoes.vista.OperacaoVista;
 
-public class Compra implements OperacaoVista {
+public class Compra extends OperacaoVista {
+
+    @Override
+    public BigDecimal calculaValorOperacao() {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
     
     
