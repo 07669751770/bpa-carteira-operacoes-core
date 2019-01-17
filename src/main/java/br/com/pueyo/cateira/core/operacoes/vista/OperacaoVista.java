d4cd42a62c668e85956fff0e9531a4366be986f8
BrunoNit <pueyonit@gmail.com>
2019-01-17 16:22:08 -0200

    Nova funcionalidade

--- src/main/java/br/com/pueyo/cateira/core/operacoes/vista/OperacaoVista.java
+++ src/main/java/br/com/pueyo/cateira/core/operacoes/vista/OperacaoVista.java
@@ -1,5 +1,37 @@
 package br.com.pueyo.cateira.core.operacoes.vista;
 
-public interface OperacaoVista {
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
+public abstract class OperacaoVista {
+
+    protected BigDecimal taxaCorretagem;
+    protected BigDecimal taxaLiquidacao;
+    protected BigDecimal taxaRegistro;
+    protected BigDecimal emolumentos;
+    protected BigInteger quantidade;
+    
+    public void comTaxaCorretagem(BigDecimal taxaCorretagem) {
+        this.taxaCorretagem = taxaCorretagem;
+    }
+    public void comTaxaLiquidacao(BigDecimal taxaLiquidacao) {
+        this.taxaLiquidacao = taxaLiquidacao;
+    }
+    public void comTaxaRegistro(BigDecimal taxaRegistro) {
+        this.taxaRegistro = taxaRegistro;
+    }
+    public void comEmolumentos(BigDecimal emolumentos) {
+        this.emolumentos = emolumentos;
+    }
+    public void comQuantidade(BigInteger quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+    public abstract BigDecimal calculaValorOperacao();
+    
+    
+    
+    
+    
+    
 }
