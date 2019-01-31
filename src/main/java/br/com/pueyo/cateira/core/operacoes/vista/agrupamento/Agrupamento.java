52abe544a4ea10f99cbce9f772c1fa2cbcd5c1c6
BrunoNit <pueyonit@gmail.com>
2019-01-31 18:50:50 -0200

    Casca de Novas Operações

---
+++ src/main/java/br/com/pueyo/cateira/core/operacoes/vista/agrupamento/Agrupamento.java
@@ -0,0 +1,19 @@
+package br.com.pueyo.cateira.core.operacoes.vista.agrupamento;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import br.com.pueyo.cateira.core.operacoes.vista.OperacaoVista;
+
+public class Agrupamento extends OperacaoVista {
+    
+    List<OperacaoVista> listaOperacaoVista;
+
+    @Override
+    public BigDecimal calculaValorOperacao() {
+        return null;
+    }
+    
+    
+
+}
---
+++ src/main/java/br/com/pueyo/cateira/core/operacoes/vista/desdobramento/Desdobramento.java
@@ -0,0 +1,19 @@
+package br.com.pueyo.cateira.core.operacoes.vista.desdobramento;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import br.com.pueyo.cateira.core.operacoes.vista.OperacaoVista;
+
+public class Desdobramento extends OperacaoVista {
+    
+    List<OperacaoVista> listaOperacaoVista;
+
+    @Override
+    public BigDecimal calculaValorOperacao() {
+        return null;
+    }
+    
+    
+
+}
